37ed0eacc910faf92f9a387d85625e27a18aa3f0
Markku2809 <markkuinvest@gmail.com>
2025-10-08 10:09:20 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// Aseta base repon nimen mukaan:
 export default defineConfig({
   plugins: [react()],
+  base: '/nordic-sheet-metal-vite/',
 })
